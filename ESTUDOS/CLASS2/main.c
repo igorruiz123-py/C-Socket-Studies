097d02e0c0ec2f685aa87e88116681eb643aa574
Igor Ruiz <ruizigor08@gmail.com>
2026-01-14 00:06:03 -0300

    Adicionando comentário no código

--- ESTUDOS/CLASS2/main.c
+++ ESTUDOS/CLASS2/main.c
@@ -50,36 +50,51 @@ NULL -> uses local IP
 
 int main(){
 
+    // status variable. It will hold 0 for success
     int status;
 
+    // hints: (IPv4 or IPv6) | serverinfo: (linked list returned by the OS) | p: (pointer to iterate the list) 
     struct addrinfo hints, *serverinfo, *p;
+
+    // Buffer to keep the IP (big enough for IPv6)
     char ipstr[INET6_ADDRSTRLEN];
 
+    // Freeing hints to avoid memory trash
     memset(&hints, 0, sizeof hints);
 
+    // Configuring filters (AF_UNSPEC -> IPv4 or IPv6)
     hints.ai_family = AF_UNSPEC;
+    // We wanto TCP
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = AI_PASSIVE;
 
-
-    if ((status = getaddrinfo("google.com", "2100", &hints, &serverinfo)) != 0) {
+    // This block does:
+    //  1. Resolve DNS (google.com)
+    //  2. Checks IPV4 / IPv6 if available
+    //  3. Consider type TCP
+    //  4. Return a linked list of address
+    if ((status = getaddrinfo("google.com", "http", &hints, &serverinfo)) != 0) {
         fprintf(stderr, "gai error: %s\n", gai_strerror(status));
         exit(1);
     }
 
-    printf("Address returned by the OS: \n\n");
-
+    // Loop to iterate the linked list
     for (p = serverinfo; p != NULL; p = p->ai_next){
 
-        void * addr;
-        char *ip;
+        // Auxiliar pointers
+        void * addr; // Generic IP for IPv4 and IPv6
+        char *ip; // String for IPv4 and IPv6
 
+        // If addreess be IPv4
         if (p->ai_family == AF_INET){
+            // Convert generic sockaddr to sockaddr_in  
             struct sockaddr_in *ipv4 = (struct sockaddr_in *)p->ai_addr;
+            // Takes only IPv4 addresses
             addr = &(ipv4->sin_addr);
             ip = "IPv4";
         }
         
+        // Same logic
         else{
 
             struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)p->ai_addr;
@@ -87,7 +102,9 @@ int main(){
             ip = "IPv6";
         }
 
+        // Convert binary IP to string
         inet_ntop(p->ai_family, addr, ipstr, sizeof ipstr);
+        // Print output
         printf("  %s: %s\n", ip, ipstr);
     }
 
